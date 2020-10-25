c023279ab06fc6e2078d8db25a851c5d409d2b09
Darylabrador <abrador.daryl@gmail.com>
2020-10-25 20:08:01 +0400

    first commit

---
+++ app/Models/Assign.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Assign extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'date', 'hours'
+    ];
+
+    public function clients()
+    {
+        return $this->belongsTo('App\Models\Client');
+    }
+
+    public function desktops()
+    {
+        return $this->belongsTo('App\Models\Desktop');
+    }
+}
---
+++ database/migrations/2020_10_25_155904_create_assigns_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assigns', function (Blueprint $table) {
+            $table->id();
+            $table->integer('client_id', null, false)->index();
+            $table->integer('desktop_id', null, false)->index();
+            $table->dateTime('date');
+            $table->string('hours');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assigns');
+    }
+}
